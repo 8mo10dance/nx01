62479d326439363d0f077bd2f12e8af216b474c2
tomoya sunda <dexn.dev@outlook.jp>
2024-03-17 19:24:46 +0900

    (#153) chore: index.html を Webpack で生成する

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
@@ -34,4 +35,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    })
+  ]
 }
