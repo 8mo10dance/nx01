9c39bfd73559dbd192ba264bb68f63040897c90b
tomoya sunda <dexn.dev@outlook.jp>
2024-05-10 12:32:58 +0900

    feat: migrate

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_20_124017) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_10_032908) do
   create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uuid"
+    t.index ["uuid"], name: "index_todos_on_uuid", unique: true
   end
 
 end
